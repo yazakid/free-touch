a41ad090f84a1aa81b17849c318571edcd253f17
yazakid <15012992791@163.com>
2019-03-19 17:31:29 +0800

    add template

---
+++ src/components/template/template.js
@@ -0,0 +1,13 @@
+import  Taro,{Component}  from  '@tarojs/taro';
+import {View,Text,Image} from '@tarojs/components';
+import './template.less'
+class  Template  extends  Component{
+	constructor(){
+		super(...arguments);
+		this.state={};
+	}
+	render(){
+	   return  (<View>Template</View>)	
+	}
+}
+export default Template;
