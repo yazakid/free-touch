b9b5bba887083854e8c8a243f97bbb70b45c2544
yazakid <15012992791@163.com>
2019-04-08 17:02:21 +0800

    test config

--- src/app.js
+++ src/app.js
@@ -18,34 +18,50 @@ const store = configStore()
 
 class App extends Component {
   config = {
+    //用于指定小程序由哪些页面组成
     pages: [
       'pages/home/home',
       'pages/webview/webview'
     ],
+
+    //window:用于设置小程序的状态栏、导航条、标题、窗口背景色
     window: {
-      backgroundTextStyle: 'light',
-      navigationBarBackgroundColor: '#fff',
-      navigationBarTitleText: 'free-touch',
-      navigationBarTextStyle: 'black'
+      //RN支持
+      navigationBarBackgroundColor: '#fff',//导航栏背景颜色
+      navigationBarTitleText: 'anydoor',//导航栏标题文字内容
+      navigationBarTextStyle: 'black',//导航栏标题颜色，仅支持 black / white
+      //RN不支持
+      backgroundTextStyle: 'light',//下拉 loading 的样式，仅支持 dark / light
     },
+
+    //客户端窗口的底部或顶部有 tab 栏可以切换页面
     tabBar: {
-      color: "#666",
-      selectedColor: "#b4282d",
-      backgroundColor: "#fafafa",
-      borderStyle: 'black',
+      //RN支持,且必填
+      color: "#666",//tab 上的文字默认颜色
+      selectedColor: "#b4282d",//tab 上的文字选中时的颜色
+      backgroundColor: "#fafafa",//tab 的背景色
+      borderStyle: 'black',//tabbar 上边框的颜色， 仅支持 black / white
+      //tab 的列表，最少 2 个、最多 5 个 tab
       list: [{
+        //必填
         pagePath: "pages/home/home",
+        text: "首页",
+        //非必填
         iconPath: "./assets/images/home.png",
-        selectedIconPath: "./assets/images/home-active.png",
-        text: "首页"
+        selectedIconPath: "./assets/images/home-active.png"
       }, {
         pagePath: "pages/home/home",
+        text: " tabBar",
         iconPath: "./assets/images/star.png",
-        selectedIconPath: "./assets/images/star.png",
-        text: " tabBar"
-      }]
+        selectedIconPath: "./assets/images/star.png"
+      }],
+      //RN不支持
+      positon:'',
+      custom:'',
     }
   }
+  //RN不支持此生命周期，在微信/百度/字节跳动/支付宝小程序中这一生命周期方法对应 app 的 onLaunch
+  componentWillMount() {}
 
   componentDidMount () {}
 
